2dba379e3338393cf86c79804ea272fb809cdba4
Wanzenböck, Ralf <ralf.wanzenboeck@tuwien.ac.at>
2023-06-07 10:26:57 +0200

    delete all files

--- docs/source/modules.rst
+++
@@ -1,7 +0,0 @@
-clinamen2
-=========
-
-.. toctree::
-   :maxdepth: 4
-
-   clinamen2
--- examples/scheduler_start_nwchem.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dask-scheduler --port 0 --scheduler-file scheduler_nwchem.json --interface em2 1>LOG 2>LOGERR
